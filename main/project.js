74a5f1a31f221f66f020b885ba8375fa7a1bd1f0
AidenBulwinkle <jo.joe.moe7@gmail.com>
2023-05-29 16:35:11 -0700

    Single Button (not working)

--- main/project.js
+++ main/project.js
@@ -19,3 +19,22 @@ function main() {
 
 // let's get this party started
 main();
+
+
+// Single button open and close
+
+function openPopup() {
+  $('.popup').css('display', 'block');
+  $("p, h2, button").addClass("open-popup"); //sets to block so popup is visible 
+  $("p, h2, button").removeClass("popup");
+}
+
+function closePopup() {
+  $('.popup').css('display', 'none'); //closes popup, sets state to none
+  $("p, h2, button").addClass("popup");
+  $("p, h2, button").removeClass("open-popup");
+}
+
+function changeImage() {
+  $('.button1 img').attr('src', 'img/untiled.jpg');
+  $("img").addClass("popup-img")} //changes image from environment.jpg to IMG
